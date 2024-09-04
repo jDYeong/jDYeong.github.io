35f13279ab79913e97399486a28f99a003c1dcac
jDYeong <ddajung97@gmail.com>
2024-09-04 19:29:29 +0900

    Updates

---
+++ precache-manifest.ccdc14356b85fc4dcabc3ea231ca8c0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbcee6af8d20d46d265a91585b546a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc4d4dc9ce1aaf772607",
+    "url": "/static/js/2.f7d36e02.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.f7d36e02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8d4a0a768ba791cac6",
+    "url": "/static/js/3.3f55c254.chunk.js"
+  },
+  {
+    "revision": "afba789eab3c7ac90904",
+    "url": "/static/js/main.91ce0cb1.chunk.js"
+  },
+  {
+    "revision": "af712d830eba538dbe1c",
+    "url": "/static/js/runtime-main.fd95531b.js"
+  }
+]);
