752516d0a7b73f8f86b8386af6803b0da772a01b
jDYeong <ddajung97@gmail.com>
2024-11-01 21:30:25 +0900

    상세페이지 닫기버튼 클릭시 목록페이지로 이동

--- dyblog/src/components/common/buttons/btnClose.js
+++ dyblog/src/components/common/buttons/btnClose.js
@@ -2,15 +2,21 @@ import React from 'react';
 import {Pc, Mobile, Tablet} from '../../../common/MediaQuery';
 import { useHistory } from "react-router-dom";
 import { ReactComponent as IconClose } from '../../../assets/images/icons/icon-close.svg';
+import { Link } from 'react-router-dom';
 
 const BtnClose = () => {
     const history = useHistory();
+    
+    let pathname = history.location.pathname;
+    let name = pathname.split('/');
+
     return (
-            <button type='button' className='btn-type1' onClick={() => history.goBack()}>
+            <Link to={`/${name[1]}`} className='btn-type1'>
+            {/* <button type='button' className='btn-type1' onClick={() => history.goBack()}> */}
                 <Pc><IconClose width={24} height={24} /></Pc>
                 <Tablet><IconClose width={20} height={20} /></Tablet>
                 <Mobile><IconClose width={20} height={20} /></Mobile>
-            </button>
+            </Link>
     );
 };
 
